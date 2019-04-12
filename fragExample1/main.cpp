f720ecb88eec3a7e63d38ffdb930ab17e1e1dc7f
liyicheng <11098758@vivo.xyz>
2019-04-12 18:18:59 +0800

    uploaded fragment shader example1 and fragment shader example 2

---
+++ fragExample1/main.cpp
@@ -0,0 +1,43 @@
+#include"base.h"
+#include"Shader.h"
+class my_application : public sb7::application
+{
+public:
+	void startup() {
+		Shader oneShader("vertexShader.vs",
+						"tessControlShader.tcs",
+						"tessEvalShader.tes",
+						"geometryShader.gs",
+						"fragmentShader.fs");
+		program = oneShader.ID;
+		glCreateVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+	}
+
+	void render(double currentTime) {
+		static const GLfloat back[] = { 0.2f,0.2f,0.2f,1.0f };
+		glClearBufferfv(GL_COLOR, 0, back);
+
+		glUseProgram(program);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
+	virtual void shutdown()
+	{
+		glDeleteVertexArrays(1, &vao);
+		glDeleteProgram(program);
+	}
+
+private:
+	GLuint          program;
+	GLuint          vao;
+};
+
+int main() {
+	my_application* app = new my_application;
+	app->run();
+	delete app;
+	return 0;
+}
+
+
---
+++ fragExample2/base.h
@@ -0,0 +1,148 @@
+﻿#ifndef __SB7_H__
+#define __SB7_H__
+
+#define Sleep(t) sleep(t)
+
+
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include<iostream>
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+namespace sb7
+{
+
+	class application
+	{
+	public:
+		application() {}
+		virtual ~application() {}
+		virtual void run()
+		{
+			bool running = true;
+
+			if (!glfwInit())
+			{
+				return;
+			}
+
+			init();
+
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+			glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+			glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+			{
+				window = glfwCreateWindow(800, 600, "opengl", NULL, NULL);
+				if (!window)
+				{
+					fprintf(stderr, "Failed to open window\n");
+					return;
+				}
+			}
+
+			glfwMakeContextCurrent(window);
+
+			// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+			glewExperimental = GL_TRUE;
+			// Initialize GLEW to setup the OpenGL Function pointers
+			if (GLEW_OK != glewInit())
+			{
+				std::cout << "Failed to initialize GLEW" << std::endl;
+				return ;
+			}
+			startup();
+			do
+			{
+				render(glfwGetTime());
+
+				glfwSwapBuffers(window);
+				glfwPollEvents();
+
+				running &= (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_RELEASE);
+				running &= (glfwWindowShouldClose(window) != GL_TRUE);
+			} while (running);
+
+			shutdown();
+
+			glfwDestroyWindow(window);
+			glfwTerminate();
+		}
+
+		virtual void init()
+		{
+		}
+
+		virtual void startup()
+		{
+
+		}
+
+		virtual void render(double currentTime)
+		{
+
+		}
+
+		virtual void shutdown()
+		{
+
+		}
+
+
+		virtual void onKey(int key, int action)
+		{
+
+		}
+
+		virtual void onMouseButton(int button, int action)
+		{
+
+		}
+
+		virtual void onMouseMove(int x, int y)
+		{
+
+		}
+
+		virtual void onMouseWheel(int pos)
+		{
+
+		}
+
+		void getMousePosition(int& x, int& y)
+		{
+			double dx, dy;
+			glfwGetCursorPos(window, &dx, &dy);
+
+			x = static_cast<int>(floor(dx));
+			y = static_cast<int>(floor(dy));
+		}
+
+
+	protected:
+		GLFWwindow* window;
+	};
+
+};
+
+/*
+#define DECLARE_MAIN(a)                             \
+sb7::application *app = 0;                          \
+int CALLBACK WinMain(HINSTANCE hInstance,           \
+                     HINSTANCE hPrevInstance,       \
+                     LPSTR lpCmdLine,               \
+                     int nCmdShow)                  \
+{                                                   \
+    a *app = new a;                                 \
+    app->run(app);                                  \
+    delete app;                                     \
+    return 0;                                       \
+}
+*/
+#endif /* __SB7_H__ */
+
